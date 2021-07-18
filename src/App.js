6aa5b36a6e6921e4dbb1410e40e0589bfa1ada6c
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-07-18 17:04:55 +0100

    adding a image slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import './App.css';
-import Footer from './footer/Footer'
+import { Navbar } from './components/navbar';
+import Footer from './footer/Footer';
 
 
 function App() {
     return (
+      <div className='app'>
+     <Navbar />
       <Footer />
   
-  );
+      </div>
+  )
 }
 
 export default App;
